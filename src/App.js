102f9ed11795e2f652dfa261062900e27ad50ad3
Hamzakhann <hamzakhann66@gmail.com>
2022-02-11 19:31:17 +0500

    added nft list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import React, { useState, useEffect } from "react";
 import Header from "./Components/Header";
 import CollectionCard from "./Components/CollectionCard";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [punkListData, setPunkLinkData] = useState(null);
+
+  useEffect(() => {
+    const getMyNfts = async () => {
+      try {
+        const response = await axios.get(
+          "https://testnets-api.opensea.io/assets?asset_contract_address=0x9e803f4F32D938174301A99D7B0Bff503c150294",
+          {
+            headers: {
+              "Content-Type": "text/plain"
+            }
+          }
+        );
+        console.log(response.data, ".........................");
+        setPunkLinkData(response.data.assets);
+      } catch (e) {
+        console.log(e, "wwwwwwwwwwwwww");
+      }
+    };
+    getMyNfts();
+  }, []);
+
   return (
     <div className="app">
       <Header />
---
+++ src/Components/Punklist.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import CollectionCard from "./CollectionCard";
+const Punklist = ({punklistData}) => {
+  return (
+    <div className='punk' >
+
+    </div>
+  )
+}
+
+export default Punklist
